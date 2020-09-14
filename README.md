6b4a855c96a6f0ad00e0d68d4d1e7988f0c1acce
Bouwe K. Westerdijk <bwesterdijk@newnexus.nl>
2020-09-14 20:24:58 +0200

    .

---
+++ README.md
@@ -0,0 +1,3 @@
+# my-KPN-react-game
+
+The game I built during my "Live Coding a Game in React" for KPN GroSkills on September 15, 2020.
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <img
+        src="https://uploads-eu-west-1.insided.com/kpn-business/attachment/4daa74a9-13fe-4df3-bf2a-fcceb353e964_thumb.png"
+        height="50"
+        alt="KPN logo"
+      />
+      <img
+        src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K"
+        height="47"
+        alt="React logo"
+      />
+      <h1>Live Coding a Game in React</h1>
+      <p>Welkom allemaal!</p>
+    </div>
+  );
+}
+
+export default App;
